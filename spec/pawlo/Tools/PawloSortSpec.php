dcebf2f6e8c7138b5a4e95f606954496b5bddcfa
Pawel Czuchaj <pawlo1501@gmail.com>
2015-05-17 20:28:01 +0200

    Metody

--- spec/pawlo/Tools/PawloSortSpec.php
+++ spec/pawlo/Tools/PawloSortSpec.php
@@ -11,4 +11,12 @@ class PawloSortSpec extends ObjectBehavior
     {
         $this->shouldHaveType('pawlo\Tools\PawloSort');
     }
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setStr('Kotek')->getStr()->shouldReturn('Kotek');
+    }
+      function it_should_sort_by_length()
+    {
+        $this->setStr('kotek ali')->sort()->shouldReturn('kotek -5 ali -3');
+    }
 }
--- src/pawlo/Tools/PawloSort.php
+++ src/pawlo/Tools/PawloSort.php
@@ -4,4 +4,61 @@ namespace pawlo\Tools;
 
 class PawloSort
 {
+    private $str;
+   
+    public function setStr($str)
+    {
+        $this->str = $str;
+
+        return $this;
+    }
+
+    public function getStr()
+    {
+        return $this->str;
+    }
+    
+    public function sort()
+    {
+        $dlugosc=strlen($this->str);
+        $i=0;
+        $pom;
+        $aTab=array();
+        
+        for ($k=0;$k<$dlugosc;$k++)
+        {
+            if ($str[k]!= ' ')
+            {
+                $pom+=$str[k];
+            }
+            else if ($str[k]== ' ')
+            {
+                $aTab[]=$pom;
+                $pom=' ';
+            }
+            
+        }
+        
+        $i=count($aTab);
+        for($j=0;$j<$i;$j++)
+        {
+             for($k=0;$k<$i-1;$k++)
+             {
+                  if(strlen($aTab[k])>(strlen($aTab[k+1])))
+                  {
+                $pom=$aTab[k];
+                $aTab[k]=$aTab[k+1];
+                $aTab[k+1]=$pom;
+                 }
+             }
+        
+        }
+        $pom='';
+          for($j=0;$j<$i;$j++)
+          {
+              $pom+=$aTab[j];
+              $pom+=' -'.count($aTab[j]);
+          }
+          return $pom;
+    }
 }
