ae0af88c9ad92a23b2583d0e4142ecffd47e6d92
Damian Pyc <pycu0007@gmail.com>
2015-05-19 16:59:32 +0200

    Zadanie1: implementacja metod

--- spec/dpyc/Tools/Zadanie1Spec.php
+++ spec/dpyc/Tools/Zadanie1Spec.php
@@ -11,4 +11,14 @@ class Zadanie1Spec extends ObjectBehavior
     {
         $this->shouldHaveType('dpyc\Tools\Zadanie1');
     }
+    
+    function it_should_have_setter_and_getter()
+    {
+        $this->setStr('damian ma kota damian ma')->getStr()->shouldReturn('damian ma kota damian ma');
+    }
+    
+    function it_should_generate_wynik()
+    {
+        $this->setStr('damian ma kota damian ma')->wynik()->shouldReturn('damian - 2; ma - 2; kota - 1; ');
+    }
 }
--- src/dpyc/Tools/Zadanie1.php
+++ src/dpyc/Tools/Zadanie1.php
@@ -4,4 +4,29 @@ namespace dpyc\Tools;
 
 class Zadanie1
 {
+    private $str;
+    public function setStr($str)
+    {
+        $this->str = $str;
+        return $this;
+    }
+    public function getStr()
+    {
+    	return $this->str;
+    }
+    public function wynik()
+    {
+    	$slowa = explode(" ", $this->str);
+        $tab = array_combine($slowa, array_fill(0, count($slowa), 0));
+        $wynik = '';
+        foreach($slowa as $slowo) 
+        {
+        	$tab[$slowo]++;
+		}
+		foreach($tab as $slowo => $licznik) 
+		{
+			$wynik .= "$slowo - $licznik; ";
+		}
+		return $wynik;
+    }
 }
